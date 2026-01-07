f347ca023222a84ae97bd54744a558c640cc4904
Vaibhav Kumar <vaibhavchoudhary2666@gmail.com>
2026-01-07 16:24:44 +0530

    added Getter method and Override method

--- src/classes/cat.java
+++ src/classes/cat.java
@@ -12,13 +12,27 @@ public class cat {
         this.breed=breed;
 
     }
+    public String getname() { return name;}
+    public String getcolour() { return colour;}
+    public int getage() {return age;}
+    public String getbreed() {return breed;}
+
+    @Override
+    public String toString() {
+        return "Name is :" + name +"\ncolour age and breed are :" + colour +" " + age +" " + breed;
+    }
+
     public static void main(String[] args) {
-        cat puccy=new cat("puccy","orange",4,"orange");
+        cat puccy=new cat("Puccy","orange",4,"Prussian");
         System.out.println(puccy);
         System.out.println(puccy.name);
         System.out.println(puccy.colour);
         System.out.println(puccy.age);
         System.out.println(puccy.breed);
+        System.out.print(puccy.getname()+"\t");
+        System.out.print(puccy.getage()+"\t");
+        System.out.print(puccy.getbreed()+ "\t");
+        System.out.print(puccy.getcolour()+"\t");
 
     }
 }
